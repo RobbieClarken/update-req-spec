11c1596d57acc40b927e230e80455e69568ce103
RobbieClarken <robbie.clarken@gmail.com>
2019-02-07 21:38:53 +1100

    Implement SourceReplacer to update setup.py

--- src/repin/repin.py
+++ src/repin/repin.py
@@ -9,6 +9,33 @@ def repin_file(filename):
     node_finder.visit(ast.parse(source))
     if node_finder.node is None:
         raise RuntimeError("could not find install_requires keyword")
+    for node in reversed(node_finder.node.value.elts):
+        new_text = (
+            "channelarchiver>=0.1.0,<2" if node.s.startswith("c") else "ttmltosrt>0.5,<2"
+        )
+        source = SourceReplacer(source, node).replace_with(new_text)
+    path.write_text(source)
+
+
+class SourceReplacer:
+    def __init__(self, source, node):
+        self._source = source
+        self._node = node
+
+    def replace_with(self, new_text):
+        return self._text_before + new_text + self._text_after
+
+    @property
+    def _text_before(self):
+        lines = self._source.splitlines()[: self._node.lineno]
+        lines[-1] = lines[-1][: self._node.col_offset + 1]
+        return "\n".join(lines)
+
+    @property
+    def _text_after(self):
+        lines = self._source.splitlines()[self._node.lineno - 1 :]
+        lines[0] = lines[0][self._node.col_offset + 1 + len(self._node.s) :]
+        return "\n".join(lines)
 
 
 class InstallRequiresNodeFinder(ast.NodeVisitor):
--- tests/test_repin.py
+++ tests/test_repin.py
@@ -14,10 +14,71 @@ def test_repin_raises_runtime_error_if_cant_find_install_requires(tmp_path):
             setup(
                 name="example",
                 version="0.0.0",
-            )
-            """
+            )"""
         )
     )
     with pytest.raises(RuntimeError) as exc_info:
         repin_file(str(setup_file))
     assert "could not find install_requires keyword" in str(exc_info.value)
+
+
+def test_repin(tmp_path):
+    setup_file = tmp_path / "setup.py"
+    setup_file.write_text(
+        dedent(
+            """\
+            from setuptools import setup
+            setup(
+                name="example",
+                version="0.0.0",
+                install_requires=[
+                    "channelarchiver~=0.1.0",
+                    "ttmltosrt>0.5",
+                ],
+            )"""
+        )
+    )
+    repin_file(str(setup_file))
+    expected_text = dedent(
+        """\
+        from setuptools import setup
+        setup(
+            name="example",
+            version="0.0.0",
+            install_requires=[
+                "channelarchiver>=0.1.0,<2",
+                "ttmltosrt>0.5,<2",
+            ],
+        )"""
+    )
+    assert setup_file.read_text() == expected_text
+
+
+def test_repin_when_requirements_on_same_line(tmp_path):
+    setup_file = tmp_path / "setup.py"
+    setup_file.write_text(
+        dedent(
+            """\
+            from setuptools import setup
+            setup(
+                name="example",
+                version="0.0.0",
+                install_requires=[
+                    "channelarchiver~=0.1.0", "ttmltosrt>0.5",
+                ],
+            )"""
+        )
+    )
+    repin_file(str(setup_file))
+    expected_text = dedent(
+        """\
+        from setuptools import setup
+        setup(
+            name="example",
+            version="0.0.0",
+            install_requires=[
+                "channelarchiver>=0.1.0,<2", "ttmltosrt>0.5,<2",
+            ],
+        )"""
+    )
+    assert setup_file.read_text() == expected_text
