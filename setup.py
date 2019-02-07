8ff4f1fb11b2289ae8c423a7b70560f3f85ce486
RobbieClarken <robbie.clarken@gmail.com>
2019-02-07 19:53:10 +1100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import re
+
+from setuptools import setup
+
+with open("src/repin/__init__.py") as file:
+    version = re.search(r'__version__ = "(.*)"', file.read()).group(1)
+
+
+setup(
+    name="re-pin",
+    version=version,
+    license="MIT",
+    author="Robbie Clarken",
+    author_email="robert.clarken@reece.com.au",
+    package_dir={"": "src"},
+    packages=["repin"],
+    install_requires=["pip-tools"],
+)
